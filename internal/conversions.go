6ddec9b8c0d7704b9c3632f1b55e6ee1126f4323
oduortoni <oduortoni@gmail.com>
2025-03-13 20:34:48 +0300

    feat: entropy and stringarraytoint conversion
    entropy is a function to determine the entropy of a dataset
    stringarraytoint conversion is used to cenvert an array of strings to an array of integers

---
+++ internal/conversions.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+* StringArrayToOrdinal - converts an array of strings to an array of integers
+* the integers will be assigned as first come basis. All strings that are equal will have the same integer value
+* input: 
+*  an array of strings
+*
+* output:
+*  an array of integers with substituted integer values
+*/
+func StringArrayToOrdinal(arr []string) []int {
+	index := 1
+	m := make(map[string]int)
+	norminal := []int{}
+
+	for _, item := range arr {
+		if v, ok := m[item]; ok {
+			norminal = append(norminal, v)
+		} else {
+			m[item] = index
+			index++
+		}
+	}
+	return norminal
+}
