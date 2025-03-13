2645dcea4dcfdcd529f8290478b04448ce97bc06
tesiaka <tsiaka7197@stu.kemu.ac.ke>
2025-03-13 22:02:34 +0300

    chore:function to store trained data

---
+++ internal/modalstorage/store.go
@@ -0,0 +1,20 @@
+package modalstorage
+
+import (
+	"encoding/json"
+	"os"
+
+	internal "github.com/nyunja/c45-decision-tree/internal"
+)
+
+func SaveModel(tree *internal.DecisionTree) error {
+	file, err := os.Create("./models/store.dt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(tree)
+}
