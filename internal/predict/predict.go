8717110e9088bd428ec6d99950ca4e5b53a78de4
davodhiambo <jesseomolo@yahoo.com>
2025-03-14 15:34:44 +0300

    feat: extract data from input and outpul files.

--- internal/predict/predict.go
+++ internal/predict/predict.go
@@ -1,5 +1,18 @@
 package predict
 
-func Predic(modelFile, inputFile, outputFile string) {
+import dataprocessor "github.com/nyunja/c45-decision-tree/internal/dataProcessor"
 
+func Predic(modelFile, inputFile, outputFile string) error {
+	// load data
+	model, err := dataprocessor.ReadJSONFile(modelFile)
+	if err != nil {
+		return err
+	}
+
+	input, err := dataprocessor.ReadCSVFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
