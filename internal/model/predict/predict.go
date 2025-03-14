1adc1957cbf4bc44190ef152c77bf4da63d0469e
oduortoni <oduortoni@gmail.com>
2025-03-15 02:47:47 +0300

    feat: prediction
    add a prediction feature
    uses the model generated by the trainer to make predictions of the attribute used to train the model

---
+++ internal/model/predict/predict.go
@@ -0,0 +1,83 @@
+package predict
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"runtime"
+	"sync"
+
+	t "github.com/nyunja/c45-decision-tree/internal/model/types"
+)
+
+// BatchPredict makes predictions for multiple instances in parallel
+func BatchPredict(model *t.Model, instances []t.Instance) []string {
+	predictions := make([]string, len(instances))
+
+	fmt.Println("INSTANCES: ", instances)
+
+	// Use a worker pool to make predictions in parallel
+	numWorkers := runtime.NumCPU()
+	instancesChan := make(chan int, len(instances))
+	var wg sync.WaitGroup
+
+	// Start worker goroutines
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for idx := range instancesChan {
+				predictions[idx] = PredictClass(model, instances[idx])
+			}
+		}()
+	}
+
+	// Send instances to workers
+	go func() {
+		for i := range instances {
+			instancesChan <- i
+		}
+		close(instancesChan)
+	}()
+
+	// Wait for all workers to finish
+	wg.Wait()
+
+	return predictions
+}
+
+// SavePredictions saves predictions to a CSV file
+func SavePredictions(instances []t.Instance, predictions []string, filename string, headers []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write the header
+	predictionHeaders := append([]string{}, headers...)
+	predictionHeaders = append(predictionHeaders, "prediction")
+	err = writer.Write(predictionHeaders)
+	if err != nil {
+		return fmt.Errorf("error writing header: %v", err)
+	}
+
+	// Write the predictions
+	for i, instance := range instances {
+		row := make([]string, len(headers)+1)
+		for j, header := range headers {
+			row[j] = fmt.Sprintf("%v", instance[header])
+		}
+		row[len(headers)] = predictions[i]
+
+		err = writer.Write(row)
+		if err != nil {
+			return fmt.Errorf("error writing prediction: %v", err)
+		}
+	}
+
+	return nil
+}
---
+++ internal/model/predict/predict_class.go
@@ -0,0 +1,89 @@
+package predict
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	t "github.com/nyunja/c45-decision-tree/internal/model/types"
+	ndp "github.com/nyunja/c45-decision-tree/internal/model/node"
+)
+
+// PredictClass predicts the class of an instance
+func PredictClass(model *t.Model, instance t.Instance) string {
+	node := model.Root
+	for !node.IsLeaf {
+		feature := node.Feature
+		val, ok := instance[feature]
+		if !ok || val == nil {
+			// Handle missing value
+			// Find the most common class among children
+			classCounts := make(map[string]int)
+			for _, child := range node.Children {
+				if child.IsLeaf {
+					classCounts[child.Class]++
+				}
+			}
+			majorityClass := ""
+			maxCount := 0
+			for class, count := range classCounts {
+				if count > maxCount {
+					maxCount = count
+					majorityClass = class
+				}
+			}
+			return majorityClass
+		}
+
+		if node.Continuous {
+			var floatVal float64
+			switch v := val.(type) {
+			case float64:
+				floatVal = v
+			case int:
+				floatVal = float64(v)
+			case time.Time:
+				floatVal = float64(v.Unix())
+			default:
+				strVal := fmt.Sprintf("%v", val)
+				parsedVal, err := strconv.ParseFloat(strVal, 64)
+				if err != nil {
+					// Can't convert to float, use majority class
+					return ndp.GetMajorityClassFromNode(node)
+				}
+				floatVal = parsedVal
+			}
+
+			if floatVal <= node.Threshold {
+				node = node.Children[0] // Left child
+			} else {
+				node = node.Children[1] // Right child
+			}
+		} else {
+			strVal := fmt.Sprintf("%v", val)
+			found := false
+			for _, child := range node.Children {
+				childVal := fmt.Sprintf("%v", child.Value)
+				if childVal == strVal {
+					node = child
+					found = true
+					break
+				}
+			}
+			if !found {
+				// If value not found in any child, use the "unknown" branch or majority class
+				for _, child := range node.Children {
+					if fmt.Sprintf("%v", child.Value) == "unknown" {
+						node = child
+						found = true
+						break
+					}
+				}
+				if !found {
+					return ndp.GetMajorityClassFromNode(node)
+				}
+			}
+		}
+	}
+	return node.Class
+}
